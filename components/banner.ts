fac1f85035d13a0914d1a5834451f26cb24bad72
Dolkip <dolkip.sm@gmail.com>
2026-05-16 14:14:40 +0100

    e

---
+++ components/banner.ts
@@ -0,0 +1,25 @@
+import { BoxRenderable, TextRenderable } from "@opentui/core"
+import { Theme } from "../theme"
+import { renderer } from "../renderer"
+
+export const banner = new BoxRenderable(renderer, {
+    id: "banner",
+    width: "100%",
+    height: 1,
+    flexDirection: "row",
+})
+
+export const bannerTitle = new TextRenderable(renderer, {
+    id: "banner-title",
+    content: "◐ Shell",
+    fg: Theme.text,
+})
+
+export const bannerText = new TextRenderable(renderer, {
+    id: "banner-text",
+    content: " is a tiny Discord terminal client",
+    fg: Theme.dim,
+})
+
+banner.add(bannerTitle)
+banner.add(bannerText)
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { BoxRenderable } from "@opentui/core"
 import { renderer } from "./renderer"
 import { messageBox, textArea } from "./components/messagebox"
+import { banner } from "./components/banner"
 
 const app = new BoxRenderable(renderer, {
   id: "app",
@@ -20,6 +21,9 @@ const main = new BoxRenderable(renderer, {
 
 app.add(main)
 app.add(messageBox)
+app.add(banner)
 
 renderer.root.add(app)
+
+//focuses on the text area so you start typing right away
 textArea.focus()
